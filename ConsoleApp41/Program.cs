ba389d67e087bc4ddd84db90ba37c6de23693a35
sulton <global-sultonmalik326@gmail.com>
2024-01-12 03:12:18 +0500

    Add project files.

---
+++ ConsoleApp41/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Text;
+using System.Text.Json;
+
+
+namespace ConsoleApp41
+{
+    public class Program
+    {
+    /*    static void Main(string[] args)
+        {
+
+           
+               var postData = new PostData
+               {
+                   PostId = 1,
+                   Id=501,
+                   Name = "Sirdarya",
+                   Email = "jdjddjjd.com",
+                   Body="jsjdjdj"
+
+               };
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+
+            var json = JsonSerializer.Serialize(postData);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = client.PostAsync("posts", content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+                var postResponse = JsonSerializer.Deserialize<PostResponse>(responseContent);
+                Console.WriteLine(postResponse);
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+            }
+        }
+*/
+
+        /* Bu post qismi */
+
+        /* Get qismi*/
+
+
+        /*     public static HttpClient sharedClient = new()
+             {
+                 BaseAddress = new Uri("https://jsonplaceholder.typicode.com")
+
+
+
+             };
+
+       static async Task Main(string[] args)
+       {
+
+           await HttpMethods.GetAsync(sharedClient);
+
+
+
+
+
+       }*/
+
+
+        /* Get qismi*/
+
+
+        /*Delete qismi*/
+
+        /*   public static HttpClient sharedClient = new()
+           {
+               BaseAddress = new Uri("https://jsonplaceholder.typicode.com")
+
+
+
+           };
+
+           static async Task Main(string[] args)
+           {
+
+               await DeleteData.DeleteAsync(sharedClient);
+
+
+
+
+
+           }*/
+
+
+
+
+
+
+    }
+
+    /*Delete qismi*/
+
+
+
+
+
+}
+/*}*/
